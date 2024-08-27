18474264e3d06a6886e14aff8a378f04466394ea
David <davidm435465123@gmail.com>
2024-08-26 19:34:04 -0500

    Se ajustaron carpetas

--- src/components/auth/RegistroCliente.js
+++ src/components/auth/RegistroCliente.js
@@ -26,6 +26,7 @@ const RegistroCliente = () => {
         try {
             await axios.post('http://localhost:5000/usuarios', datosFormulario);
             alert('Cliente registrado con éxito');           
+            navigate('/login'); // Redirige al usuario después del registro
         } catch (error) {
             console.error('Error al registrar el cliente', error);
         }
@@ -33,18 +34,18 @@ const RegistroCliente = () => {
 
     return (
         <div>
-            <section class="bg-gray-50">
-                <div class="flex flex-col items-center justify-center px-6 py-16 mx-auto md:h-screen lg:py-0">
-                    <div class="w-full bg-white rounded-lg shadow-md md:mt-0 sm:max-w-md xl:p-0">
-                        <div class="p-6 space-y-4 md:space-y-6 sm:p-8">
-                            <h1 class="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl">
+            <section className="bg-gray-50">
+                <div className="flex flex-col items-center justify-center px-6 py-16 mx-auto md:h-screen lg:py-0">
+                    <div className="w-full bg-white rounded-lg shadow-md md:mt-0 sm:max-w-md xl:p-0">
+                        <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
+                            <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl">
                                 Crea tu cuenta
                             </h1>                           
-                            <form class="space-y-4 md:space-y-6" onSubmit={manejarEnvio}>
-                                <div>
+                            <form className="space-y-4 md:space-y-6 grid grid-cols-2 gap-4" onSubmit={manejarEnvio}>
+                                <div className="col-span-2">
                                     <label
                                         htmlFor="correo"
-                                        class="block mb-2 text-sm font-medium text-gray-900"
+                                        className="block mb-2 text-sm font-medium text-gray-900"
                                     >
                                         Correo Electrónico
                                     </label>
@@ -54,7 +55,7 @@ const RegistroCliente = () => {
                                         id="correo"
                                         value={datosFormulario.correo}
                                         onChange={manejarCambio}
-                                        class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5"
+                                        className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5"
                                         placeholder="nombre@gmail.com"
                                         required
                                     />
@@ -62,7 +63,7 @@ const RegistroCliente = () => {
                                 <div>
                                     <label
                                         htmlFor="nombreUsuario"
-                                        class="block mb-2 text-sm font-medium text-gray-900"
+                                        className="block mb-2 text-sm font-medium text-gray-900"
                                     >
                                         Nombre de Usuario
                                     </label>
@@ -72,7 +73,7 @@ const RegistroCliente = () => {
                                         id="nombreUsuario"
                                         value={datosFormulario.nombreUsuario}
                                         onChange={manejarCambio}
-                                        class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5"
+                                        className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5"
                                         placeholder="Tu nombre de usuario"
                                         required
                                     />
@@ -80,7 +81,7 @@ const RegistroCliente = () => {
                                 <div>
                                     <label
                                         htmlFor="nombre"
-                                        class="block mb-2 text-sm font-medium text-gray-900"
+                                        className="block mb-2 text-sm font-medium text-gray-900"
                                     >
                                         Nombre
                                     </label>
@@ -90,7 +91,7 @@ const RegistroCliente = () => {
                                         id="nombre"
                                         value={datosFormulario.nombre}
                                         onChange={manejarCambio}
-                                        class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5"
+                                        className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5"
                                         placeholder="Tu nombre completo"
                                         required
                                     />
@@ -98,7 +99,7 @@ const RegistroCliente = () => {
                                 <div>
                                     <label
                                         htmlFor="telefono"
-                                        class="block mb-2 text-sm font-medium text-gray-900"
+                                        className="block mb-2 text-sm font-medium text-gray-900"
                                     >
                                         Teléfono
                                     </label>
@@ -108,7 +109,7 @@ const RegistroCliente = () => {
                                         id="telefono"
                                         value={datosFormulario.telefono}
                                         onChange={manejarCambio}
-                                        class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5"
+                                        className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5"
                                         placeholder="3123456789"
                                         required
                                     />
@@ -116,7 +117,7 @@ const RegistroCliente = () => {
                                 <div>
                                     <label
                                         htmlFor="direccion"
-                                        class="block mb-2 text-sm font-medium text-gray-900"
+                                        className="block mb-2 text-sm font-medium text-gray-900"
                                     >
                                         Dirección
                                     </label>
@@ -126,15 +127,15 @@ const RegistroCliente = () => {
                                         id="direccion"
                                         value={datosFormulario.direccion}
                                         onChange={manejarCambio}
-                                        class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5"
+                                        className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5"
                                         placeholder="Tu dirección"
                                         required
                                     />
                                 </div>
-                                <div>
+                                <div className="col-span-2">
                                     <label
                                         htmlFor="contrasena"
-                                        class="block mb-2 text-sm font-medium text-gray-900"
+                                        className="block mb-2 text-sm font-medium text-gray-900"
                                     >
                                         Contraseña
                                     </label>
@@ -145,30 +146,30 @@ const RegistroCliente = () => {
                                         value={datosFormulario.contrasena}
                                         onChange={manejarCambio}
                                         placeholder="••••••••"
-                                        class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5"
+                                        className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5"
                                         required
                                     />
                                 </div>
                                 <input type="hidden" name="rol" value={datosFormulario.rol} />
                                 <input type="hidden" name="estado" value={datosFormulario.estado} /> {/* Campo oculto para el estado */}
-                                <div class="flex items-start">
-                                    <div class="flex items-center h-5">
+                                <div className="flex items-start col-span-2">
+                                    <div className="flex items-center h-5">
                                         <input
                                             id="terminos"
                                             aria-describedby="terminos"
                                             type="checkbox"
-                                            class="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300"
+                                            className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300"
                                             required
                                         />
                                     </div>
-                                    <div class="ml-3 text-sm">
+                                    <div className="ml-3 text-sm">
                                         <label
                                             htmlFor="terminos"
-                                            class="font-light text-gray-500"
+                                            className="font-light text-gray-500"
                                         >
                                             Acepto los{" "}
                                             <a
-                                                class="font-medium text-primary-600 hover:underline"
+                                                className="font-medium text-primary-600 hover:underline"
                                                 href="#"
                                             >
                                                 Términos y Condiciones
@@ -176,7 +177,7 @@ const RegistroCliente = () => {
                                         </label>
                                     </div>
                                 </div>
-                                <button type="submit" class="px-8 py-4 bg-gradient-to-r from-blue-500 to-purple-500 text-white font-bold rounded-full transition-transform transform-gpu hover:-translate-y-1 hover:shadow-lg">
+                                <button type="submit" className="px-8 py-4 bg-gradient-to-r from-blue-500 to-purple-500 text-white font-bold rounded-full transition-transform transform-gpu hover:-translate-y-1 hover:shadow-lg col-span-2">
                                     Crear
                                 </button>
                             </form>
