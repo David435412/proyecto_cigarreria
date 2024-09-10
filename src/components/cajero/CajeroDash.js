a74e1fb95b56e1a0c254a6366676348f5a993f09
DarwinL14 <dslesmes6@soy.sena.edu.co>
2024-09-09 20:57:44 -0500

    Se modificaron todos los dashboard

--- src/components/cajero/CajeroDash.js
+++ src/components/cajero/CajeroDash.js
@@ -53,85 +53,87 @@ const CajeroDashboard = () => {
     }, []);
 
     return (
+
+        <>
+        <div className="bg-green-600 text-white p-6 shadow-md mt-5 ">
+            <h2 className="text-center mb-4 text-4xl font-bold">¡Bienvenido, {userName}!</h2>
+            <p className="text-center mb-4 text-xl">
+            Este es tu dashboard, donde puedes gestionar productos, proveedores, ventas y pedidos.
+            </p>
+        </div>
         <div className="container mx-auto px-4 py-8">
-            {outOfStockAlerts.length > 0 && (
-                <div className="bg-red-100 text-red-800 p-4 rounded-lg mb-8">
-                    <h3 className="text-lg font-bold">Alerta de Producto Agotado</h3>
-                    <ul>
-                        {outOfStockAlerts.map((product) => (
-                            <li key={product.id} className="mb-2">
-                                <strong>{product.nombre}</strong> - Se ha agotado. El estado ha sido cambiado a inactivo. 
-                                Es recomendable agregar más cantidad de este producto.
-                            </li>
-                        ))}
-                    </ul>
-                    <div className="mt-4">
-                        <Link
-                            to="/productos-agotados-cajero"
-                            className="text-blue-500 hover:underline"
-                        >
-                            Ver Productos Agotados
-                        </Link>
+                {outOfStockAlerts.length > 0 && (
+                    <div className="bg-red-100 text-red-800 p-4 rounded-lg mb-8">
+                        <h3 className="text-lg font-bold">Alerta de Producto Agotado</h3>
+                        <ul>
+                            {outOfStockAlerts.map((product) => (
+                                <li key={product.id} className="mb-2">
+                                    <strong>{product.nombre}</strong> - Se ha agotado. El estado ha sido cambiado a inactivo.
+                                    Es recomendable agregar más cantidad de este producto.
+                                </li>
+                            ))}
+                        </ul>
+                        <div className="mt-4">
+                            <Link
+                                to="/productos-agotados-cajero"
+                                className="text-blue-500 hover:underline"
+                            >
+                                Ver Productos Agotados
+                            </Link>
+                        </div>
+                    </div>
+                )}
+
+                {lowStockProducts.length > 0 && (
+                    <div className="bg-yellow-100 text-yellow-800 p-4 rounded-lg mb-8">
+                        <h3 className="text-lg font-bold">Alerta de Stock Bajo</h3>
+                        <ul>
+                            {lowStockProducts.map((product) => (
+                                <li key={product.id} className="mb-2">
+                                    <strong>{product.nombre}</strong> - Stock bajo ({product.cantidad})
+                                </li>
+                            ))}
+                        </ul>
+                    </div>
+                )}
+
+
+                <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-6 mb-8">
+                    {/* Gestión de Productos */}
+                    <div className="bg-white p-6 shadow-md rounded-lg">
+                        <h2 className="text-xl font-bold mb-2">Gestión de Productos</h2>
+                        <p className="text-gray-700">
+                            Administra los productos en el sistema. Podrás realizar todas las operaciones CRUD para añadir y actualizar productos del inventario.
+                        </p>
+                    </div>
+
+                    {/* Gestión de Proveedores */}
+                    <div className="bg-white p-6 shadow-md rounded-lg">
+                        <h2 className="text-xl font-bold mb-2">Gestión de Proveedores</h2>
+                        <p className="text-gray-700">
+                            Administra a los proveedores. Podrás realizar todas las operaciones CRUD para gestionar la información de los proveedores del sistema.
+                        </p>
                     </div>
                 </div>
-            )}
 
-            {lowStockProducts.length > 0 && (
-                <div className="bg-yellow-100 text-yellow-800 p-4 rounded-lg mb-8">
-                    <h3 className="text-lg font-bold">Alerta de Stock Bajo</h3>
-                    <ul>
-                        {lowStockProducts.map((product) => (
-                            <li key={product.id} className="mb-2">
-                                <strong>{product.nombre}</strong> - Stock bajo ({product.cantidad})
-                            </li>
-                        ))}
-                    </ul>
-                </div>
-            )}
+                <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-6 mb-8">
+                    {/* Ventas */}
+                    <div className="bg-white p-6 shadow-md rounded-lg">
+                        <h2 className="text-xl font-bold mb-2">Ventas</h2>
+                        <p className="text-gray-700">
+                            Registra y consulta las ventas realizadas en el sistema. Podrás gestionar el historial de ventas.
+                        </p>
+                    </div>
 
-            <div className="bg-white p-6 shadow-md rounded-lg mb-8">
-                <h2 className="text-2xl font-bold mb-2">¡Bienvenido, {userName}!</h2>
-                <p className="text-gray-700">
-                    Este es tu dashboard, donde puedes gestionar productos, proveedores, ventas y pedidos. 
-                </p>
-            </div>
-
-            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-6 mb-8">
-                {/* Gestión de Productos */}
-                <div className="bg-white p-6 shadow-md rounded-lg">
-                    <h2 className="text-xl font-bold mb-2">Gestión de Productos</h2>
-                    <p className="text-gray-700">
-                        Administra los productos en el sistema. Podrás realizar todas las operaciones CRUD para añadir y actualizar productos del inventario.
-                    </p>
+                    {/* Pedidos */}
+                    <div className="bg-white p-6 shadow-md rounded-lg">
+                        <h2 className="text-xl font-bold mb-2">Pedidos</h2>
+                        <p className="text-gray-700">
+                            Consulta el estado de los pedidos realizados por los clientes. Podrás visualizar el seguimiento adecuado.
+                        </p>
+                    </div>
                 </div>
-                
-                {/* Gestión de Proveedores */}
-                <div className="bg-white p-6 shadow-md rounded-lg">
-                    <h2 className="text-xl font-bold mb-2">Gestión de Proveedores</h2>
-                    <p className="text-gray-700">
-                        Administra a los proveedores. Podrás realizar todas las operaciones CRUD para gestionar la información de los proveedores del sistema.
-                    </p>
-                </div>
-            </div>
-
-            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-6 mb-8">
-                {/* Ventas */}
-                <div className="bg-white p-6 shadow-md rounded-lg">
-                    <h2 className="text-xl font-bold mb-2">Ventas</h2>
-                    <p className="text-gray-700">
-                        Registra y consulta las ventas realizadas en el sistema. Podrás gestionar el historial de ventas.
-                    </p>
-                </div>
-
-                {/* Pedidos */}
-                <div className="bg-white p-6 shadow-md rounded-lg">
-                    <h2 className="text-xl font-bold mb-2">Pedidos</h2>
-                    <p className="text-gray-700">
-                        Consulta el estado de los pedidos realizados por los clientes. Podrás visualizar el seguimiento adecuado.
-                    </p>
-                </div>
-            </div>
-        </div>
+            </div></>
     );
 };
 
