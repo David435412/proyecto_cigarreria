5f3a209e9b70e851d70639bf5fc92e5c78bbcf20
DarwinL14 <dslesmes6@soy.sena.edu.co>
2024-09-13 19:53:41 -0500

    ultimos ajustes para el pull

--- src/components/administrador/AdminDash.js
+++ src/components/administrador/AdminDash.js
@@ -1,7 +1,8 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import { Link } from 'react-router-dom';
-
+import fuera_4 from "../../assets/images/fuera_4.jpeg";
+import css from "../../pages/css.css";
 
 const AdminDashboard = () => {
     const [userName, setUserName] = useState('');
@@ -55,27 +56,33 @@ const AdminDashboard = () => {
     }, []);
 
     return (
-
         <>
-        <div className="bg-green-600 text-white p-6 shadow-md mt-5 ">
-            <h2 className="text-center mb-4 text-4xl font-bold">¡Bienvenido, {userName}!</h2>
-            <p className="text-center mb-4 text-xl">
-                Este es tu dashboard, donde puedes gestionar todo el sistema de manera integral. Desde aquí podrás administrar usuarios, productos, proveedores, ventas y pedidos.
-            </p>
-        </div>
-        <div className="container mx-auto px-4 py-8">
-                {outOfStockAlerts.length > 0 && (
-                    <div className="bg-red-100 text-red-800 p-4 rounded-lg mb-8">
-                        <h3 className="text-lg font-bold">Alerta de Producto Agotado</h3>
-                        <ul>
-                            {outOfStockAlerts.map((product) => (
-                                <li key={product.id} className="mb-2">
-                                    <strong>{product.nombre}</strong> - Se ha agotado. El estado ha sido cambiado a inactivo.
-                                    Es recomendable agregar más cantidad de este producto.
-                                </li>
-                            ))}
-                        </ul>
-                        <div className="mt-4">
+            <div className="bg-black text-white pb-5">
+                <img 
+                    src={fuera_4} 
+                    alt="Fondo" 
+                    className="w-full h-96 object-cover filter imagen brightness-50"
+                />
+                <div className="container mx-auto text-center">
+                    <h1 className="text-4xl font-bold">¡Bienvenido, {userName}!</h1>
+                    <p className="text-xl mt-2">Este es tu dashboard, donde puedes gestionar todo el sistema de manera integral. Desde aquí podrás administrar usuarios, productos, proveedores, ventas y pedidos.</p>
+                </div>
+            </div>
+
+            <div className="container mx-auto px-4 py-8">
+                {/* Alerts Section */}
+                <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-6 mb-8">
+                    {outOfStockAlerts.length > 0 && (
+                        <div className="bg-red-100 text-red-800 p-4 rounded-lg flex flex-col justify-between shadow-md">
+                            <h3 className="text-lg font-bold">Alerta de Producto Agotado</h3>
+                            <ul className="mb-4">
+                                {outOfStockAlerts.map((product) => (
+                                    <li key={product.id} className="mb-2">
+                                        <strong>{product.nombre}</strong> - Se ha agotado. El estado ha sido cambiado a inactivo.
+                                        Es recomendable agregar más cantidad de este producto.
+                                    </li>
+                                ))}
+                            </ul>
                             <Link
                                 to="/productos-agotados"
                                 className="text-blue-500 hover:underline"
@@ -83,62 +90,60 @@ const AdminDashboard = () => {
                                 Ver Productos Agotados
                             </Link>
                         </div>
-                    </div>
-                )}
+                    )}
 
-                {lowStockProducts.length > 0 && (
-                    <div className="bg-yellow-100 text-yellow-800 p-4 rounded-lg mb-8">
-                        <h3 className="text-lg font-bold">Alerta de Stock Bajo</h3>
-                        <ul>
-                            {lowStockProducts.map((product) => (
-                                <li key={product.id} className="mb-2">
-                                    <strong>{product.nombre}</strong> - Stock bajo ({product.cantidad})
-                                </li>
-                            ))}
-                        </ul>
-                    </div>
-                )}
+                    {lowStockProducts.length > 0 && (
+                        <div className="bg-yellow-100 text-yellow-800 p-4 rounded-lg flex flex-col justify-between shadow-md">
+                            <h3 className="text-lg font-bold">Alerta de Stock Bajo</h3>
+                            <ul className="mb-4">
+                                {lowStockProducts.map((product) => (
+                                    <li key={product.id} className="mb-2">
+                                        <strong>{product.nombre}</strong> - Stock bajo ({product.cantidad})
+                                    </li>
+                                ))}
+                            </ul>
+                        </div>
+                    )}
+                </div>
 
-
-                <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-6 mb-8">
-                    <div className="bg-white p-6 shadow-md rounded-lg">
+                {/* Management Cards Section */}
+                <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 mb-8">
+                    <div className="bg-white p-6 shadow-md rounded-lg flex flex-col justify-between h-full">
                         <h2 className="text-xl font-bold mb-2">Gestión de Usuarios</h2>
-                        <p className="text-gray-700">
+                        <p className="text-gray-700 flex-grow">
                             Administra a los usuarios del sistema. Podrás realizar todas las operaciones CRUD (crear, leer, actualizar, eliminar) para los roles de cajero y domiciliario.
                         </p>
                     </div>
-                    <div className="bg-white p-6 shadow-md rounded-lg">
+                    <div className="bg-white p-6 shadow-md rounded-lg flex flex-col justify-between h-full">
                         <h2 className="text-xl font-bold mb-2">Gestión de Productos</h2>
-                        <p className="text-gray-700">
+                        <p className="text-gray-700 flex-grow">
                             Administra los productos en el sistema. Podrás realizar todas las operaciones CRUD para añadir, actualizar o eliminar productos del inventario.
                         </p>
                     </div>
-                </div>
 
-                <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-6 mb-8">
-                    <div className="bg-white p-6 shadow-md rounded-lg">
+                    <div className="bg-white p-6 shadow-md rounded-lg flex flex-col justify-between h-full">
                         <h2 className="text-xl font-bold mb-2">Gestión de Proveedores</h2>
-                        <p className="text-gray-700">
+                        <p className="text-gray-700 flex-grow">
                             Administra a los proveedores. Podrás realizar todas las operaciones CRUD para gestionar la información de los proveedores del sistema.
                         </p>
                     </div>
-                    <div className="bg-white p-6 shadow-md rounded-lg">
+                    <div className="bg-white p-6 shadow-md rounded-lg flex flex-col justify-between h-full">
                         <h2 className="text-xl font-bold mb-2">Ventas</h2>
-                        <p className="text-gray-700">
+                        <p className="text-gray-700 flex-grow">
                             Registra, consulta e inactiva ventas realizadas en el sistema. Podrás gestionar el historial de ventas y su estado.
                         </p>
                     </div>
-                </div>
-
-                <div className="grid grid-cols-1 gap-6 mb-8">
-                    <div className="bg-white p-6 shadow-md rounded-lg">
+                    <div className="bg-white p-6 shadow-md rounded-lg flex flex-col justify-between h-full">
                         <h2 className="text-xl font-bold mb-2">Pedidos</h2>
-                        <p className="text-gray-700">
+                        <p className="text-gray-700 flex-grow">
                             Consulta y modifica el estado de los pedidos realizados por los clientes. Gestiona el campo de estado de entrega para asegurar un seguimiento adecuado.
                         </p>
                     </div>
                 </div>
-            </div></>
+
+                    
+            </div>
+        </>
     );
 };
 
--- src/components/cliente/ClienteDash.js
+++ src/components/cliente/ClienteDash.js
@@ -2,6 +2,8 @@ import React, { useEffect, useState } from 'react';
 import { useNavigate } from "react-router-dom";
 import axios from 'axios';
 import { Link } from 'react-router-dom';
+import fuera_1 from "../../assets/images/fuera_4.jpeg";
+import css from "../../pages/css.css";
 
 const categorias = [
     'Licores',
@@ -17,34 +19,65 @@ const categorias = [
 
 const ClienteDashboard = () => {
     const [productos, setProductos] = useState([]);
+    const [productosMasVendidos, setProductosMasVendidos] = useState([]);
     const [categoriaSeleccionada, setCategoriaSeleccionada] = useState('');
     const [filteredProducts, setFilteredProducts] = useState([]);
     const [error, setError] = useState(null);
     const [userName, setUserName] = useState('');
     const navigate = useNavigate(); // Para la redirección
 
-    useEffect(() => {    
+    useEffect(() => {
         const storedName = localStorage.getItem('name');
         if (storedName) {
             setUserName(storedName);
         }
 
-        const fetchProductos = async () => {
+        const fetchDatos = async () => {
             try {
-                const response = await axios.get('http://localhost:5000/productos');
-                const productosActivos = response.data.filter(producto => producto.estado === 'activo');
+                const productosResponse = await axios.get('http://localhost:5000/productos');
+                const pedidosResponse = await axios.get('http://localhost:5000/pedidos');
+
+                const productosActivos = productosResponse.data.filter(producto => producto.estado === 'activo');
                 setProductos(productosActivos);
-                setFilteredProducts(productosActivos); 
+
+                // Contar la cantidad total vendida de cada producto
+                const conteoProductos = {};
+                pedidosResponse.data.forEach(pedido => {
+                    pedido.productos.forEach(producto => {
+                        if (!conteoProductos[producto.id]) {
+                            conteoProductos[producto.id] = {
+                                ...producto,
+                                cantidadTotal: 0
+                            };
+                        }
+                        conteoProductos[producto.id].cantidadTotal += producto.cantidad;
+                    });
+                });
+
+                // Convertir el objeto a un array y ordenar por cantidad total vendida
+                const productosMasVendidosArray = Object.values(conteoProductos);
+                productosMasVendidosArray.sort((a, b) => b.cantidadTotal - a.cantidadTotal);
+
+                // Obtener los primeros 5 productos más vendidos
+                setProductosMasVendidos(productosMasVendidosArray.slice(0, 4));
+                setFilteredProducts(productosActivos);
+
+                // Depuración
+                console.log('Productos activos:', productosActivos);
+                console.log('Pedidos:', pedidosResponse.data);
+                console.log('Conteo de productos:', conteoProductos);
+                console.log('Productos más vendidos:', productosMasVendidosArray);
+
             } catch (error) {
-                setError('Error al obtener los productos');
-                console.error('Error al obtener los productos', error);
+                setError('Error al obtener los datos');
+                console.error('Error al obtener los datos', error);
             }
         };
 
-        fetchProductos();
+        fetchDatos();
     }, []);
 
-    useEffect(() => {       
+    useEffect(() => {
         if (categoriaSeleccionada === '') {
             setFilteredProducts(productos);
         } else {
@@ -56,21 +89,68 @@ const ClienteDashboard = () => {
         navigate(`/Detalle/${id}`);
     };
 
+    const handleCategoriaClick = (categoria) => {
+        setCategoriaSeleccionada(categoria);
+    };
+
+    const handleVerTodosClick = () => {
+        setCategoriaSeleccionada(''); // Limpiar categoría seleccionada para mostrar todos los productos
+    };
+
     return (
-
         <>
-        <div className="bg-green-600 text-white p-6 shadow-md mt-5 mb-5">
-            <h1 className="text-center mb-4 text-4xl font-bold">
-                Bienvenido a la Cigarreria Colonial, {userName}
-            </h1>
-            <p className="text-center mb-4 text-xl">
-                En nuestra pagina podras realizar pedidos de los diversos productos que te ofrecemos
-            </p>
-        </div>
-        <div className="container mx-auto px-4 py-8">
+            <div className="bg-black text-white pb-5">
+                <img 
+                    src={fuera_1} 
+                    alt="Fondo" 
+                    className="w-full h-96 object-cover filter imagen brightness-50"
+                />
+                <div className="container mx-auto text-center">
+                    <h1 className="text-4xl font-bold">Bienvenido a la Cigarrería Colonial</h1>
+                    <p className="text-xl mt-2">Encuentra y realiza pedidos de los mejores productos</p>
+                </div>
+            </div>
 
-                <h1 className="text-3xl font-bold mt-5 mb-4">Encuentra los Mejores Productos</h1>
-                <p className="mb-8">
+            {/* Productos más vendidos */}
+            <div className="container mx-auto px-4 py-8">
+                <h2 className="text-3xl font-bold mt-5 mb-4 text-center">Productos más vendidos</h2>
+                <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
+                    {productosMasVendidos.length > 0 ? (
+                        productosMasVendidos.map(producto => (
+                            <Link
+                                to={`/producto/${producto.id}`}
+                                key={producto.id}
+                                className="bg-white border border-gray-200 rounded-lg shadow-md overflow-hidden flex flex-col"
+                            >
+                                <div className="w-full h-64 relative flex-shrink-0">
+                                    <img
+                                        src={producto.imagen}
+                                        alt={producto.nombre}
+                                        className="object-cover w-full h-full" />
+                                </div>
+                                <div className="p-4 flex flex-col justify-between flex-grow">
+                                    <div>
+                                        <h2 className="text-xl font-semibold mb-2">{producto.nombre}</h2>
+                                        <p className="text-gray-900 font-bold mb-4">${producto.precio}</p>
+                                    </div>
+                                    <button
+                                        className="w-full bg-green-600 text-white py-2 px-4 rounded hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-indigo-500"
+                                    >
+                                        Ver más
+                                    </button>
+                                </div>
+                            </Link>
+                        ))
+                    ) : (
+                        <p className="text-gray-500 text-center">No hay productos más vendidos disponibles.</p>
+                    )}
+                </div>
+            </div>
+
+            {/* Categorías y productos */}
+            <div className="container mx-auto px-4 py-8">
+                <h1 className="text-3xl font-bold mt-5 mb-4 text-center">Categorías</h1>
+                <p className="mb-8 text-center">
                     En esta sección encontrarás una amplia selección de productos.
                     Usa el filtro a continuación para encontrar exactamente lo que buscas por categoría.
                 </p>
@@ -78,17 +158,24 @@ const ClienteDashboard = () => {
                 {error && <p className="text-red-500 mb-4">{error}</p>}
 
                 {/* Filtros */}
-                <div className="mb-6 flex space-x-4">
-                    <select
-                        value={categoriaSeleccionada}
-                        onChange={(e) => setCategoriaSeleccionada(e.target.value)}
-                        className="p-2 border border-gray-300 rounded"
+                <div className="mb-6 flex flex-wrap justify-center gap-4">
+                    {/* Círculo para mostrar todos los productos */}
+                    <div
+                        onClick={handleVerTodosClick}
+                        className={`bg-gray-300 cursor-pointer p-3 border rounded-full shadow-xl text-sm font-medium text-center transition-transform duration-300 ease-in-out w-24 h-24 flex items-center justify-center transform ${categoriaSeleccionada === '' ? 'bg-green-500 text-white border-green-500' : 'bg-gray-100 text-black border-gray-300'} hover:bg-green-500 hover:text-white hover:scale-110 hover:-translate-y-2 hover:shadow-2xl`}
                     >
-                        <option value="">Seleccionar categoría</option>
-                        {categorias.map(categoria => (
-                            <option key={categoria} value={categoria}>{categoria}</option>
-                        ))}
-                    </select>
+                        Todos
+                    </div>
+
+                    {categorias.map(categoria => (
+                        <div
+                            key={categoria}
+                            onClick={() => handleCategoriaClick(categoria)}
+                            className={`bg-gray-300 cursor-pointer p-3 border rounded-full shadow-xl text-sm font-medium text-center transition-transform duration-300 ease-in-out w-24 h-24 flex items-center justify-center transform ${categoriaSeleccionada === categoria ? 'bg-green-500 text-white border-green-500' : 'bg-gray-100 text-black border-gray-300'} hover:bg-green-500 hover:text-white hover:scale-110 hover:-translate-y-2 hover:shadow-2xl`}
+                        >
+                            {categoria}
+                        </div>
+                    ))}
                 </div>
 
                 {/* Productos */}
@@ -118,10 +205,11 @@ const ClienteDashboard = () => {
                             </Link>
                         ))
                     ) : (
-                        <p className="text-gray-500">No hay productos disponibles.</p>
+                        <p className="text-gray-500">No hay productos disponibles en esta categoría.</p>
                     )}
                 </div>
-            </div></>
+            </div>
+        </>
     );
 };
 
